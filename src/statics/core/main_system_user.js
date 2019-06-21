76ca2876f3d95ad0e6eac0e2e9450c2893bcf1ec
Vicsen Morantes <sir.vamb@gmail.com>
2019-06-21 15:47:47 -0500

    fix

--- src/statics/core/main_system_user.js
+++ src/statics/core/main_system_user.js
@@ -1,6 +1,6 @@
 window.pcsphp = {}
 window.pcsphp.authenticator = new PiecesPHPSystemUserHelper('users/login', 'users/verify')
 
-window.onload = () => {
+window.addEventListener('load', function (e) {
 	window.pcsphp.authenticator.setTriggerLogout('[pcsphp-users-logout]')
-}
+})
