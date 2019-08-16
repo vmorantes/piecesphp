7f5be7d40fc23a22161ea1f42c1b2723500b74e8
Vicsen Morantes <sir.vamb@gmail.com>
2019-08-16 11:26:51 -0500

    Controlador y estilos básicos para parte pública

---
+++ src/app/controller/PublicAreaController.php
@@ -0,0 +1,195 @@
+<?php
+
+/**
+ * PublicAreaController.php
+ */
+
+namespace App\Controller;
+
+use App\Model\AvatarModel;
+use PiecesPHP\Core\BaseHashEncryption;
+use PiecesPHP\Core\Roles;
+use PiecesPHP\Core\Route;
+use PiecesPHP\Core\RouteGroup;
+use PiecesPHP\Core\Utilities\OsTicket\OsTicketAPI;
+use \Slim\Http\Request as Request;
+use \Slim\Http\Response as Response;
+
+/**
+ * PublicAreaController.
+ *
+ * Controlador del área pública
+ *
+ * @package     App\Controller
+ * @author      Vicsen Morantes <sir.vamb@gmail.com>
+ * @copyright   Copyright (c) 2019
+ */
+class PublicAreaController extends \PiecesPHP\Core\BaseController
+{
+
+    /**
+     * $prefixNameRoutes
+     *
+     * @var string
+     */
+    private static $prefixNameRoutes = 'public';
+
+    /**
+     * $startSegmentRoutes
+     *
+     * @var string
+     */
+    private static $startSegmentRoutes = '';
+
+    /**
+     * $user
+     *
+     * Usuario logueado
+     *
+     * @var \stdClass
+     */
+    protected $user = null;
+
+    /**
+     * __construct
+     *
+     * @return static
+     */
+    public function __construct()
+    {
+        parent::__construct(false); //No cargar ningún modelo automáticamente
+        $this->init();
+        add_global_asset(base_url('statics/css/global.css'), 'css');
+    }
+
+    /**
+     * indexView
+     *
+     * Vista principal
+     *
+     * @param Request $req
+     * @param Response $res
+     * @param array $args
+     * @return void
+     */
+    public function indexView(Request $req, Response $res, array $args)
+    {
+
+        $this->render('layout/header');
+        $this->render('pages/sample-public');
+        $this->render('layout/footer');
+
+        return $res;
+    }
+
+    /**
+     * routeName
+     *
+     * @param string $name
+     * @param array $params
+     * @param bool $silentOnNotExists
+     * @return string
+     */
+    public static function routeName(string $name = null, array $params = [], bool $silentOnNotExists = false)
+    {
+        if (!is_null($name)) {
+            $name = trim($name);
+            $name = strlen($name) > 0 ? "-{$name}" : '';
+        }
+
+        $name = !is_null($name) ? self::$prefixNameRoutes . $name : self::$prefixNameRoutes;
+
+        $allowed = false;
+        $current_user = get_config('current_user');
+
+        if ($current_user != false) {
+            $allowed = Roles::hasPermissions($name, (int) $current_user->type);
+        } else {
+            $allowed = true;
+        }
+
+        if ($allowed) {
+            return get_route(
+                $name,
+                $params,
+                $silentOnNotExists
+            );
+        } else {
+            return '';
+        }
+    }
+
+    /**
+     * routes
+     *
+     * @param RouteGroup $group
+     * @return RouteGroup
+     */
+    public static function routes(RouteGroup $group)
+    {
+
+        $groupSegmentURL = $group->getGroupSegment();
+
+        $lastIsBar = last_char($groupSegmentURL) == '/';
+        $startRoute = $lastIsBar ? '' : '/';
+
+        //Otras rutas
+        $namePrefix = self::$prefixNameRoutes;
+        $startRoute .= self::$startSegmentRoutes;
+
+        //──── GET ─────────────────────────────────────────────────────────────────────────
+
+        //Generales
+        $group->register([
+            new Route(
+                "{$startRoute}[/]",
+                self::class . ":indexView",
+                "{$namePrefix}-index",
+                'GET'
+            ),
+        ]);
+
+        //──── POST ─────────────────────────────────────────────────────────────────────────
+
+        return $group;
+    }
+
+    /**
+     * init
+     *
+     * @return void
+     */
+    protected function init()
+    {
+        $api_url = get_config('osTicketAPI');
+        $api_key = get_config('osTicketAPIKey');
+
+        OsTicketAPI::setBaseURL($api_url);
+        OsTicketAPI::setBaseAPIKey($api_key);
+
+        $view_data = [];
+        $this->user = get_config('current_user');
+
+        if ($this->user instanceof \stdClass) {
+            $view_data['user'] = $this->user;
+            $this->user->avatar = AvatarModel::getAvatar($this->user->id);
+            $this->user->hasAvatar = !is_null($this->user->avatar);
+            $this->user->id = BaseHashEncryption::encrypt(base64_encode($this->user->id), self::class);
+            unset($this->user->password);
+        }
+
+        $this->setVariables($view_data);
+
+        /* JQuery */
+        import_jquery();
+        /* Semantic */
+        import_semantic();
+        /* NProgress */
+        import_nprogress();
+        /* izitoast */
+        import_izitoast();
+        /* Librerías de la aplicación */
+        import_app_libraries();
+
+    }
+}
---
+++ src/app/view/pages/sample-public.php
@@ -0,0 +1,136 @@
+<?php defined("BASEPATH") or die("<h1>El script no puede ser accedido directamente</h1>");?>
+
+<br>
+<br>
+<br>
+<br>
+
+<div class="elements-container centered fit">
+
+	<h3>Colores</h3>
+
+	<br>
+
+	<div class="elements-container small centered" style="display:flex; flex-wrap: wrap;">
+		<div style="padding:5px;background-color:rgb(2, 61, 86);width: 150px;height: 150px;color:white;">
+			<strong>Primero</strong>
+			<br>
+			<strong>rgb(2, 61, 86)</strong>
+		</div>
+		<div style="padding:5px;background-color:rgb(40, 100, 133);width: 150px;height: 150px;color:white;">
+			<strong>Segundo</strong>
+			<br>
+			<strong>rgb(40, 100, 133)</strong>
+		</div>
+		<div style="padding:5px;background-color:rgb(150, 150, 150);width: 150px;height: 150px;color:white;">
+			<strong>Tercero</strong>
+			<br>
+			<strong>rgb(150, 150, 150)</strong>
+		</div>
+		<div style="padding:5px;background-color:rgb(70, 70, 70);width: 150px;height: 150px;color:white;">
+			<strong>Cuarto</strong>
+			<br>
+			<strong>rgb(70, 70, 70)</strong>
+		</div>
+		<div style="padding:5px;background-color:rgb(30, 30, 30);width: 150px;height: 150px;color:white;">
+			<strong>Quinto</strong>
+			<br>
+			<strong>rgb(30, 30, 30)</strong>
+		</div>
+		<div style="padding:5px;background-color:white;width: 150px;height: 150px;color:black;">
+			<strong>Color para sobrponer sobre el Primero</strong>
+			<br>
+			<strong>white</strong>
+		</div>
+		<div style="padding:5px;background-color:#1e1e1e;width: 150px;height: 150px;color:white;">
+			<strong>H1</strong>
+			<br>
+			<strong>#1e1e1e</strong>
+		</div>
+		<div style="padding:5px;background-color:#286485;width: 150px;height: 150px;color:white;">
+			<strong>H2</strong>
+			<br>
+			<strong>#286485</strong>
+		</div>
+		<div style="padding:5px;background-color:#464646;width: 150px;height: 150px;color:white;">
+			<strong>H3...H6</strong>
+			<br>
+			<strong>#464646</strong>
+		</div>
+	</div>
+
+	<hr>
+
+	<h1>H1 lorem ipsum dolor sit amet consectetur.</h1>
+
+	<h2>H2 lorem ipsum dolor sit amet consectetur.</h2>
+
+	<h3>H3 lorem ipsum dolor sit amet consectetur.</h3>
+
+	<h4>H4 lorem ipsum dolor sit amet consectetur.</h4>
+
+	<h5>H5 lorem ipsum dolor sit amet consectetur.</h5>
+
+	<h6>H6 lorem ipsum dolor sit amet consectetur.</h6>
+
+	<hr>
+
+	<p class="text-align-c">P - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error aliquid unde iste ab id, pariatur sed neque fuga obcaecati eos. Magni eligendi libero odit debitis ratione dicta numquam accusamus ullam, quaerat aspernatur nobis placeat, ducimus harum fugiat voluptatum quibusdam porro natus, beatae alias iure commodi officia vel itaque.</p>
+
+	<p class="text-align-l">P - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error aliquid unde iste ab id, pariatur sed neque fuga obcaecati eos. Magni eligendi libero odit debitis ratione dicta numquam accusamus ullam, quaerat aspernatur nobis placeat, ducimus harum fugiat voluptatum quibusdam porro natus, beatae alias iure commodi officia vel itaque.</p>
+
+	<p class="text-align-r">P - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error aliquid unde iste ab id, pariatur sed neque fuga obcaecati eos. Magni eligendi libero odit debitis ratione dicta numquam accusamus ullam, quaerat aspernatur nobis placeat, ducimus harum fugiat voluptatum quibusdam porro natus, beatae alias iure commodi officia vel itaque.</p>
+
+	<p class="text-align-j">P - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error aliquid unde iste ab id, pariatur sed neque fuga obcaecati eos. Magni eligendi libero odit debitis ratione dicta numquam accusamus ullam, quaerat aspernatur nobis placeat, ducimus harum fugiat voluptatum quibusdam porro natus, beatae alias iure commodi officia vel itaque.</p>
+
+	<hr>
+
+	<ul class="pcs-list">
+
+		<li>UL - Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus ipsa nobis numquam excepturi, tenetur iusto deserunt vitae sit ratione praesentium laboriosam natus sint iure illo cupiditate veniam aperiam. Nesciunt, ad.</li>
+
+		<li>Odit nemo, fugit modi beatae at, adipisci ad nihil possimus labore quia repellat maiores, praesentium cupiditate hic illum unde ratione magni. Blanditiis, eum. Atque aperiam incidunt doloribus doloremque. Perspiciatis, doloremque.</li>
+
+		<li>Assumenda incidunt, unde laborum porro error cupiditate quidem corporis nam consectetur blanditiis quas? Maiores, ullam expedita! Recusandae voluptates fugit odio! Suscipit molestias voluptates enim nam quo error laborum ratione dolor?</li>
+
+		<li>Unde ullam esse iusto aperiam exercitationem amet sapiente impedit nisi alias similique animi facere sunt excepturi, aut pariatur porro. Exercitationem, dicta iure. Natus quis placeat magni ea animi nisi reiciendis!</li>
+
+		<li>Molestiae repudiandae ut ullam aliquam nobis officia dolores est ab ipsa voluptates asperiores amet, eaque praesentium ducimus natus. Repellat nihil et totam ducimus est quae hic ex optio esse illo!</li>
+
+		<li>Id architecto quia maiores quo nulla iusto doloremque, iste numquam, ipsum rerum assumenda sapiente culpa sint error, temporibus odio quibusdam sequi quae. Vitae ad dolores aperiam facere placeat, quidem optio.</li>
+
+	</ul>
+
+	<ol class="pcs-list">
+
+		<li>OL - Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit repudiandae nobis minima voluptates eveniet. Nesciunt blanditiis culpa ipsum cum aut esse exercitationem, atque eius, omnis ea ex quaerat eveniet minus?</li>
+
+		<li>Aliquam placeat adipisci alias soluta suscipit excepturi velit sapiente repudiandae sequi eveniet commodi, quod facere explicabo quis esse officia ipsa. Molestias voluptates doloribus dolores pariatur illo officia numquam fuga dignissimos?</li>
+
+		<li>Perferendis laboriosam totam placeat enim. Nemo, neque distinctio? Molestias dolore maxime sunt voluptatem dolorem. Praesentium nulla omnis quod quasi dolorem suscipit quia aliquid reprehenderit blanditiis, consectetur at aliquam ut molestias!</li>
+
+		<li>Accusamus error nostrum assumenda, eligendi, quod est rerum saepe nulla dignissimos eos tempore. Perferendis doloremque dicta quasi repudiandae iste vero porro placeat excepturi optio quos. Accusamus placeat et quis cumque.</li>
+
+		<li>Rem distinctio dolorum nesciunt animi magni officia, ipsum laborum eum quis quam alias porro, aperiam eos expedita perferendis error quas hic. Obcaecati cum consequatur ab recusandae, ea iusto. Quasi, obcaecati!</li>
+
+		<li>Deserunt explicabo velit fugiat ullam vero ad, veniam magni sequi minus doloremque facere qui pariatur autem. Sint aliquid doloremque deserunt ratione asperiores harum fugiat ad, mollitia voluptas corporis exercitationem ea.</li>
+
+	</ol>
+
+	<hr>
+
+	<div class="image">
+		<img src="img-gen/1920/1080" title="Imagen dentro de un div.image">
+	</div>
+
+	<p>P - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error aliquid unde iste ab id, pariatur sed neque fuga obcaecati eos. Magni eligendi libero odit debitis ratione dicta numquam accusamus ullam. Ipsam, nemo exercitationem numquam reprehenderit harum velit dignissimos sint veritatis fugiat architecto earum optio corrupti perferendis cupiditate atque labore error.</p>
+
+	<img class='block' src="img-gen/400/300" title="Imagen suelta con clase .block">
+
+	<p>P - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error aliquid unde iste ab id, pariatur sed neque fuga obcaecati eos. Magni eligendi libero odit debitis ratione dicta numquam accusamus ullam. Ipsam, nemo exercitationem numquam reprehenderit harum velit dignissimos sint veritatis fugiat architecto earum optio corrupti perferendis cupiditate atque labore error.</p>
+
+	<hr>
+
+	<blockquote>BLOCKQUOTE - Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos nemo, dolorem aut architecto libero aliquid. Quo impedit itaque totam nobis facilis voluptatem architecto porro aliquam non fugiat. Quidem, soluta voluptate.</blockquote>
+
+</div>
