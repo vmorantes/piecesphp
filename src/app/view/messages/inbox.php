e4ee0f9adfb27ee9962dfbd7a4055321f8940beb
Vicsen Morantes <sir.vamb@gmail.com>
2019-09-30 11:42:10 -0500

    Update message system

--- src/app/view/messages/inbox.php
+++ src/app/view/messages/inbox.php
@@ -1,151 +1,186 @@
 <?php
-
 use App\Controller\MessagesController as Messages;
 defined("BASEPATH") or die("<h1>El script no puede ser accedido directamente</h1>");
-
 ?>
 
-<h3 class="ui dividing header">
-    <?=__('general', 'messages');?>
-	<span class="ui mini button green" refresh-messages><i class="icon sync alternate"></i>Verificar mensajes</span>
-</h3>
-<section class="ui text container">
+<section class="ui text message-component-message-container">
+
     <div messages-component-container>
-        <div send-form>
 
-            <?php if (Messages::isAllowed($this->user->type, Messages::COMPONENT_EXTERNAL_EDITOR)): ?>
-            <div toggle-form>
-                <div>
-                    <i class="plus icon"></i>
+        <div messages-component>
+            <div aside-menu>
+                <div send-form>
+
+                    <?php if (Messages::isAllowed($this->user->type, Messages::COMPONENT_EXTERNAL_EDITOR)): ?>
+
+                    <div toggle-form>
+                        <div>
+                            <i class="plus icon"></i>
+                        </div>
+                        <div class="text">
+                            Enviar nuevo mensaje
+                        </div>
+                    </div>
+
+                    <div class="ui modal message-component-new-message-modal">
+
+                        <form messages-component-external-editor action="<?=get_route('messages-send-message');?>"
+                            method="POST" class="ui form">
+                            <p><strong>Redactar mensaje</strong></p>
+
+                            <input type="hidden" name="from" value="<?=$this->user->id;?>">
+
+                            <div class="field required">
+                                <label>Asunto</label>
+                                <input type="text" name="subject" required>
+                            </div>
+
+                            <div class="field required">
+                                <label>Mensaje</label>
+                                <input type="text" name="message" required>
+                            </div>
+
+                            <div class="field">
+                                <button type="submit" class="ui button green">Enviar</button>
+                            </div>
+
+                        </form>
+
+                    </div>
+
+                    <br>
+
+                    <?php elseif (Messages::isAllowed($this->user->type, Messages::COMPONENT_EXTERNAL_EDITOR_WITH_SELECT_DESTINE)): ?>
+
+                    <div toggle-form>
+                        <div>
+                            <i class="plus icon"></i>
+                        </div>
+                        <div class="text">
+                            Enviar nuevo mensaje
+                        </div>
+                    </div>
+
+                    <div class="ui modal message-component-new-message-modal">
+
+                        <form messages-component-external-editor action="<?=get_route('messages-send-message');?>"
+                            method="POST" class="ui form">
+                            <p><strong>Redactar mensaje</strong></p>
+
+							<input type="hidden" name="from" value="<?=$this->user->id;?>">
+							
+                            <div class="field required">
+                                <label>Para</label>
+                                <select name="to" class="ui dropdown search">
+                                    <option value="">Seleccionar destinatario</option>
+                                    <?php foreach ($destinatarios as $destinatario): ?>
+                                    <option value="<?=$destinatario->id;?>">
+                                        <?="$destinatario->firstname $destinatario->first_lastname";?>
+                                    </option>
+                                    <?php endforeach;?>
+                                </select>
+                            </div>
+
+                            <div class="field required">
+                                <label>Asunto</label>
+                                <input type="text" name="subject" required>
+                            </div>
+
+                            <div class="field required">
+                                <label>Mensaje</label>
+                                <textarea name="message" cols="30" rows="10" required></textarea>
+                            </div>
+
+                            <div class="field">
+                                <button type="submit" class="ui button green">Enviar</button>
+                            </div>
+
+                        </form>
+
+                    </div>
+                    <br>
+
+                    <?php endif;?>
+
                 </div>
-                <div class="text">
-                    Enviar nuevo mensaje
+
+                <div previews>
                 </div>
+
+                <button message-component-load-more>Cargar más</button>
+
             </div>
-            <form messages-component-external-editor action="<?=get_route('messages-send-message');?>" method="POST"
-                class="ui form">
-                <p><strong>Redactar mensaje</strong></p>
 
-                <input type="hidden" name="from" value="<?=$this->user->id;?>">
+            <div conversations></div>
 
-                <div class="field required">
-                    <label>Asunto</label>
-                    <input type="text" name="subject" required>
-                </div>
-
-                <div class="field required">
-                    <label>Mensaje</label>
-                    <input type="text" name="message" required>
-                </div>
-
-                <div class="field">
-                    <button type="submit" class="ui button green">Enviar</button>
-                </div>
-
-            </form>
-            <br>
-            <?php elseif (Messages::isAllowed($this->user->type, Messages::COMPONENT_EXTERNAL_EDITOR_WITH_SELECT_DESTINE)): ?>
-            <div toggle-form>
-                <div>
-                    <i class="plus icon"></i>
-                </div>
-                <div class="text">
-				Enviar nuevo mensaje
-                </div>
-            </div>
-            <form messages-component-external-editor action="<?=get_route('messages-send-message');?>" method="POST"
-                class="ui form">
-                <p><strong>Redactar mensaje</strong></p>
-
-                <input type="hidden" name="from" value="<?=$this->user->id;?>">
-
-                <div class="field required">
-                    <label>Para</label>
-                    <select name="to" class="ui dropdown search">
-                        <option value="">Seleccionar destinatario</option>
-                        <?php foreach ($destinatarios as $destinatario): ?>
-                        <option value="<?=$destinatario->id;?>"><?="$destinatario->firstname $destinatario->first_lastname";?>
-                        </option>
-                        <?php endforeach;?>
-                    </select>
-                </div>
-
-                <div class="field required">
-                    <label>Asunto</label>
-                    <input type="text" name="subject" required>
-                </div>
-
-                <div class="field required">
-                    <label>Mensaje</label>
-                    <input type="text" name="message" required>
-                </div>
-
-                <div class="field">
-                    <button type="submit" class="ui button green">Enviar</button>
-                </div>
-
-            </form>
-            <br>
-            <?php endif;?>
         </div>
-        <div messages-component user="<?=$this->user->id;?>"
-            route="<?=get_route('messages-inbox', ['user_id' => $this->user->id])?>">
-            <div inbox>
-                <div content></div>
-            </div>
-            <div pagination></div>
-        </div>
-
     </div>
-
 </section>
 
-<script type='text/html' messages-component-templates>
-<div message>
+<template messenger>
+
+    <div send-route="<?=get_route('messages-send-message');?>"></div>
+    <div response-route="<?=get_route('messages-send-response');?>"></div>
+    <div load-route="<?=get_route('messages-inbox', ['user_id' => $this->user->id])?>"></div>
+    <div user-id="<?=$this->user->id?>"></div>
 
     <div preview>
-        <span subject>{{subject}}</span>
-		<span date>{{date}}</span>
-		<span bubble-unread><i class="icon comment outline"></i></span>
+
+        <div class="image-container">
+            <img avatar src="<?=base_url('statics/images/default-avatar.png')?>" alt="avatar">
+        </div>
+        <div class="text-container">
+            <span date></span>
+            <span name></span>
+        </div>
+
     </div>
 
-    <div content>
+    <div main-message-body>
 
-        <div message-detail>
-            <span author>{{message_from_name}}</span>
-            <span subject>{{subject}}</span>
-            <span date>{{date}}</span>
-            <p text class="text-reading">{{message}}</p>
-        </div>
-
-        <div dialog>
-            {{messages}}
-        </div>
-
-        <form reply class="ui reply form" action="<?=get_route('messages-send-response');?>" method="POST">
-            <input type="hidden" name="message_id" value="{{message_id}}">
-            <input type="hidden" name="message_from" value="{{message_from}}">
-            <div class="field">
-                <div class="field required">
-                    <label>Escriba su respuesta</label>
-                    <textarea required name="message"></textarea>
+        <div class="details-container">
+            <div class="top-bar">
+                <button message-component-close-conversation class="ui mini button red">Cerrar</button>
+            </div>
+            <div emisor-details-container>
+                <div>
+                    <img avatar>
                 </div>
-                <div class="field">
-                    <button class="ui blue labeled submit icon button" type="submit">
-                        Enviar <i class="icon edit"></i>
-                    </button>
+                <div class="text-container">
+                    <div>
+                        <div name></div>
+                        <div course></div>
+                        <div date></div>
+                    </div>
                 </div>
             </div>
-        </form>
+        </div>
+
+        <div messages-container>
+            <h2 subject></h2>
+            <div text></div>
+        </div>
+
+        <div conversation></div>
 
     </div>
 
-</div>
-
-<div sub-message>
-    <div data>
-        <div avatar data-title="{{message_from_name}}" data-content="{{date}}">{{avatar}}</div>
+    <div sub-message>
+        <img avatar>
+        <p text></p>
     </div>
-    <div text class="text-reading">{{message}}</div>
-</div>
-</script>
+
+    <form class="ui reply form" response-message-form>
+        <input type="hidden" name="message_id">
+        <input type="hidden" name="message_from">
+        <div class="field">
+            <div class="field required">
+                <textarea name="message" placeholder="Escriba su respuesta"></textarea>
+            </div>
+            <div class="field">
+                <button class="ui blue button" type="submit">Enviar
+            </div>
+        </div>
+    </form>
+
+</template>
--- src/statics/features/messages/js/main.js
+++ src/statics/features/messages/js/main.js
@@ -1,21 +1,101 @@
-$(document).ready(function (e) {
+window.addEventListener("load", function () {
 
-	let messenger = new MessagesComponent(1, 15);
-	$('.ui.dropdown').dropdown();
+	let toggleMessageForm = $("[toggle-form]")
+	let modalNewMessage = $('.message-component-new-message-modal')
 
-	messenger.loadMessages({
-		onPreRequest: (form) => {
-			let test = $(document.createElement('input'));
-			test.attr('name', 'test');
-			test.attr('value', '123456');
-			form.append(test);
-		},
-		timeReload: 5000,
-	});
+	$(".ui.dropdown").dropdown()
 
-	let refreshButton = $('[refresh-messages]');
-	refreshButton.on('click', () => {
-		messenger.loadMessages();
-	});
+	toggleMessageForm.click(function (e) {
+		modalNewMessage.modal("show")
+	})
 
+	//Configuración de mensajes
+	let templates = document.querySelector("template[messenger]")
+
+	if (typeof MessagesComponent == "function" && templates !== null) {
+
+		templates = $(templates.content)
+		sendRoute = templates.find("[send-route]").attr("send-route")
+		loadRoute = templates.find("[load-route]").attr("load-route")
+		idSearcher = parseInt(templates.find("[user-id]").attr("user-id"))
+
+		responseRoute = templates
+			.find("[response-route]")
+			.attr("response-route")
+
+		let messenger = new MessagesComponent(1, 20, {
+			fromID: idSearcher,
+			defaultToID: null,
+			sessionToken: pcsphp.authenticator.getJWT(),
+			loadMessagesRoute: loadRoute,
+			sendMessageRoute: sendRoute,
+			sendResponseRoute: responseRoute,
+			sendFormSelector: "[messages-component-external-editor]",
+			container: "[messages-component-container] [messages-component]",
+			loadMoreButtonSelector: "[message-component-load-more]",
+			closeOnOutsideClick: false,
+			messagePreviewTemplate: {
+				template: templates.find("[preview]")
+			},
+			messageTemplate: {
+				template: templates.find("[main-message-body]")
+			},
+			subMessageTemplate: {
+				template: templates.find("[sub-message]")
+			},
+			responseFormTemplate: {
+				template: templates.find("[response-message-form]")
+			},
+			onToggle: function (preview, conversation, id) {
+
+				let isOpen = messenger.conversationIsOpened(id)
+				let windowWidth = window.innerWidth
+
+				if (windowWidth <= 1220) {
+
+					if (isOpen) {
+
+						messenger.getPreviewsContainer().hide()
+						messenger.getLoadMoreButton().hide()
+
+					} else {
+
+						messenger.getPreviewsContainer().show()
+						messenger.getLoadMoreButton().show()
+
+					}
+
+				}
+
+			},
+			onWindowResize: function (previewsContainer, conversationsContainer, lastOpened, event) {
+
+				let loadMoraButton = messenger.getLoadMoreButton()
+				let windowWidth = window.innerWidth
+
+				if (windowWidth > 1220) {
+
+					if (!previewsContainer.is(':visible')) {
+						previewsContainer.show()
+					}
+
+					if (!loadMoraButton.is(':visible')) {
+						loadMoraButton.show()
+					}
+
+				}
+
+			},
+		})
+
+		messenger.loadMessages({
+			timeReload: null
+		})
+
+		let refreshButton = $("[refresh-messages]")
+
+		refreshButton.on("click", () => {
+			messenger.loadMessages()
+		})
+	}
 })
