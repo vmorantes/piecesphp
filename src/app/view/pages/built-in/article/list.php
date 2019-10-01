27d7e95823647b382b8fc0586d79517f1c696623
Vicsen Morantes <sir.vamb@gmail.com>
2019-10-01 14:22:39 -0500

    Cambios menores

--- src/app/view/pages/built-in/article/list.php
+++ src/app/view/pages/built-in/article/list.php
@@ -1,5 +1,5 @@
 <?php defined("BASEPATH") or die("<h1>El script no puede ser accedido directamente</h1>");?>
 
 
-<div built-in-articles-items-js built-in-articles-url="<?= $ajaxURL; ?>" built-in-articles-single-url="<?= $singleURL; ?>"></div>
+<div built-in-articles-items-js built-in-articles-url="<?= $ajaxURL; ?>"></div>
 <button type="button" built-in-articles-load-more-js>Cargar más</button>
--- src/statics/js/built-in/article/list.js
+++ src/statics/js/built-in/article/list.js
@@ -1,17 +1,17 @@
-window.addEventListener('load', function(){
-	
+window.addEventListener('load', function () {
+
 	let requestURL = $('[built-in-articles-url]').attr('built-in-articles-url')
-	let singleURL = $('[built-in-articles-single-url]').attr('built-in-articles-single-url')
 
 	let articleManager = new BuiltInArticle({
 		requestURL: requestURL,
-		singleURL: singleURL,
-		page:1,
-		perPage:1,
+		page: 1,
+		perPage: 1,
 		containerSelector: '[built-in-articles-items-js]',
 		loadMoreTriggerSelector: '[built-in-articles-load-more-js]',
 		onDraw: (item) => {//Debe devolver un HTMLElement o un $|JQuery
 			console.log(item)
+			console.log(item.preferDate.date)
+			console.log(articleManager.processDate(item.preferDate.date))
 		},
 	})
 
