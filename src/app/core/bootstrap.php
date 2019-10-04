4767d165953bd86c62ca308687a47a2fc8131bc0
Vicsen Morantes <sir.vamb@gmail.com>
2019-10-04 18:03:24 -0500

    Cambio de versión

--- src/app/core/bootstrap.php
+++ src/app/core/bootstrap.php
@@ -54,7 +54,7 @@ if (!defined('APP_VERSION')) {
     /**
      * @var string Versión de la aplicación
      */
-    define('APP_VERSION', '2.9');
+    define('APP_VERSION', '3');
 }
 
 require $directories['utilities'];
