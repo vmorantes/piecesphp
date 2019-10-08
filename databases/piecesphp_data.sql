1a46265b04e234e91fdbb3b4f473848cf2911144
Vicsen Morantes <sir.vamb@gmail.com>
2019-10-08 11:14:27 -0500

    update default sql data

--- databases/piecesphp_data.sql
+++ databases/piecesphp_data.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0.1
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 18-04-2019 a las 19:05:28
--- Versión del servidor: 5.7.25-0ubuntu0.16.04.2
--- Versión de PHP: 7.1.28-1+ubuntu16.04.1+deb.sury.org+3
+-- Tiempo de generación: 08-10-2019 a las 12:12:55
+-- Versión del servidor: 5.7.27-0ubuntu0.16.04.1
+-- Versión de PHP: 7.1.32-1+ubuntu16.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -23,6 +23,38 @@ SET time_zone = "+00:00";
 --
 
 --
+-- Truncar tablas antes de insertar `locations_cities`
+--
+
+TRUNCATE TABLE `locations_cities`;
+--
+-- Truncar tablas antes de insertar `locations_countries`
+--
+
+TRUNCATE TABLE `locations_countries`;
+--
+-- Volcado de datos para la tabla `locations_countries`
+--
+
+INSERT INTO `locations_countries` (`id`, `name`, `active`) VALUES
+(1, 'Colombia', 1);
+
+--
+-- Truncar tablas antes de insertar `locations_points`
+--
+
+TRUNCATE TABLE `locations_points`;
+--
+-- Truncar tablas antes de insertar `locations_states`
+--
+
+TRUNCATE TABLE `locations_states`;
+--
+-- Truncar tablas antes de insertar `login_attempts`
+--
+
+TRUNCATE TABLE `login_attempts`;
+--
 -- Truncar tablas antes de insertar `messages`
 --
 
@@ -33,6 +65,28 @@ TRUNCATE TABLE `messages`;
 
 TRUNCATE TABLE `messages_responses`;
 --
+-- Truncar tablas antes de insertar `pcsphp_app_config`
+--
+
+TRUNCATE TABLE `pcsphp_app_config`;
+--
+-- Truncar tablas antes de insertar `pcsphp_articles`
+--
+
+TRUNCATE TABLE `pcsphp_articles`;
+--
+-- Truncar tablas antes de insertar `pcsphp_articles_categories`
+--
+
+TRUNCATE TABLE `pcsphp_articles_categories`;
+--
+-- Volcado de datos para la tabla `pcsphp_articles_categories`
+--
+
+INSERT INTO `pcsphp_articles_categories` (`id`, `name`, `description`, `friendly_url`) VALUES
+(1, 'General', '', 'general');
+
+--
 -- Truncar tablas antes de insertar `pcsphp_blackboard_news_messages`
 --
 
@@ -42,6 +96,11 @@ TRUNCATE TABLE `pcsphp_blackboard_news_messages`;
 --
 
 TRUNCATE TABLE `pcsphp_recovery_password`;
+--
+-- Truncar tablas antes de insertar `pcsphp_tickets_log`
+--
+
+TRUNCATE TABLE `pcsphp_tickets_log`;
 --
 -- Truncar tablas antes de insertar `pcsphp_tokens`
 --
@@ -57,13 +116,20 @@ TRUNCATE TABLE `pcsphp_users`;
 --
 
 INSERT INTO `pcsphp_users` (`id`, `username`, `password`, `firstname`, `secondname`, `first_lastname`, `second_lastname`, `email`, `meta`, `type`, `status`, `failed_attempts`, `created_at`, `modified_at`) VALUES
-(1, 'root', '$2y$10$7zoL4vBsF3FZ/73jSUrrD.yjYExwn3ZTIp4TsGGJr0xEaMQCmDBEK', 'Administrador', '', 'Root', '', 'vicsenmorantes@tejidodigital.com', '', 0, 1, 0, '2018-06-20 14:11:54', '2019-01-21 05:19:37');
+(1, 'root', '$2y$10$7zoL4vBsF3FZ/73jSUrrD.yjYExwn3ZTIp4TsGGJr0xEaMQCmDBEK', 'Administrador', '', 'Root', '', 'vicsenmorantes@tejidodigital.com', '', 0, 1, 0, '2018-06-20 14:11:54', '2019-01-21 05:19:37'),
+(2, 'admin', '$2y$10$hdmge3MNnbOcD5hp2OwCtuC/HzqKwkgibWGKc0hqpfn55mfRqDCfu', 'Lacey', 'Russo', 'Young', 'Curry', 'jonys@mailinator.net', NULL, 1, 1, 0, '2019-10-08 11:07:37', '2019-10-08 11:07:37'),
+(3, 'general', '$2y$10$sIZ0qYha8A/6xIv2tsatL.sDPXlkmLMZt.QqUPvNZxnlcD4cgYiuW', 'Diana', 'Donovan', 'Bean', 'Buckley', 'wynim@mailinator.com', 'null', 2, 1, 0, '2019-10-08 11:07:45', '2019-10-08 11:08:01');
 
 --
 -- Truncar tablas antes de insertar `pcsphp_user_problems`
 --
 
-TRUNCATE TABLE `pcsphp_user_problems`;COMMIT;
+TRUNCATE TABLE `pcsphp_user_problems`;
+--
+-- Truncar tablas antes de insertar `time_on_platform`
+--
+
+TRUNCATE TABLE `time_on_platform`;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
