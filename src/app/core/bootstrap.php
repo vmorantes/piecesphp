de678e35447cab546fca002509022d0829e367df
Vicsen Morantes <sir.vamb@gmail.com>
2019-10-16 17:01:00 -0500

    Versión

--- src/app/core/bootstrap.php
+++ src/app/core/bootstrap.php
@@ -54,7 +54,7 @@ if (!defined('APP_VERSION')) {
     /**
      * @var string Versión de la aplicación
      */
-    define('APP_VERSION', '3');
+    define('APP_VERSION', '3.0.1');
 }
 
 require $directories['utilities'];
