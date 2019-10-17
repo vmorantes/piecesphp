ed73b8190004030854b24cf0271e7e4f95afd718
Vicsen Morantes <sir.vamb@gmail.com>
2019-10-17 17:26:48 -0500

    i18n

--- src/app/view/usuarios/mail/other-problems.php
+++ src/app/view/usuarios/mail/other-problems.php
@@ -1,11 +1,11 @@
-<h1>Mensaje</h1>
+<h1><?= __('mailTemplates', 'Mensaje'); ?></h1>
 
-<p><strong>Asunto: <?= $subject; ?></strong></p>
-<p><strong>E-mail: <?= $mail; ?></strong></p>
-<p><strong>Nombre: <?= $name; ?></strong></p>
-<p><strong>Mensaje: <?= $message; ?></strong></p>
+<p><strong><?= __('mailTemplates', 'Asunto'); ?>: <?= $subject; ?></strong></p>
+<p><strong><?= __('mailTemplates', 'E-mail'); ?>: <?= $mail; ?></strong></p>
+<p><strong><?= __('mailTemplates', 'Nombre'); ?>: <?= $name; ?></strong></p>
+<p><strong><?= __('mailTemplates', 'Mensaje'); ?>: <?= $message; ?></strong></p>
 <?php if(isset($extra) && is_array($extra) && count($extra) > 0):?>
-<h2>Extra:</h2>
+<h2><?= __('mailTemplates', 'Extra'); ?>:</h2>
 <?php foreach($extra as $content):?>
 <p><strong><?= $content['display']; ?>: <?= $content['text']; ?></strong></p>
 <?php endforeach;?>
