eaad3829920f79aa66a0819d0ca018a46593cf79
Vicsen Morantes <sir.vamb@gmail.com>
2019-10-30 11:42:49 -0500

    --

--- tasks/TasksManager.php
+++ tasks/TasksManager.php
@@ -18,124 +18,208 @@ use PiecesPHP\Core\Helpers\Directories\FilesIgnore;
  */
 class TasksManager
 {
-	/**
-	 * task
-	 *
-	 * @param Event $event
-	 * @return void
-	 */
-	public static function task(Event $event)
-	{
-		$params_raw = $event->getArguments();
-		$params = [];
+    /**
+     * task
+     *
+     * @param Event $event
+     * @return void
+     */
+    public static function task(Event $event)
+    {
+        $params_raw = $event->getArguments();
+        $params = [];
 
-		foreach ($params_raw as $param) {
-			$param = explode('=', $param);
-			if (count($param) == 2) {
-				$name = $param[0];
-				$value = $param[1];
-				$params[$name] = $value;
-			}
-		}
+        foreach ($params_raw as $param) {
+            $param = explode('=', $param);
+            if (count($param) == 2) {
+                $name = $param[0];
+                $value = $param[1];
+                $params[$name] = $value;
+            }
+        }
 
-		if (isset($params['task']) && is_string($params['task'])) {
+        if (isset($params['task']) && is_string($params['task'])) {
 
-			$task = $params['task'];
-			unset($params['task']);
+            $task = $params['task'];
+            unset($params['task']);
 
-			if (method_exists(TasksManager::class, $task)) {
-				call_user_func(TasksManager::class . '::' . $task, $params);
-			} else {
-				echo "\r\nLa tarea $task no existe\r\n\r\n";
-			}
-		} else {
-			echo "\r\nTareas disponibles:\r\n\r\n";
-			echo "- bundle [zip=yes|no] [verbose=yes|no] \r\n";
-			echo "- srcChmod [verbose=yes|no] \r\n";
-			echo "\r\n";
-		}
-	}
+            if (method_exists(TasksManager::class, $task)) {
+                call_user_func(TasksManager::class . '::' . $task, $params);
+            } else {
+                echo "\r\nLa tarea $task no existe\r\n\r\n";
+            }
+        } else {
+            echo "\r\nTareas disponibles:\r\n\r\n";
+            echo "- bundle [zip=yes|no] [verbose=yes|no] \r\n";
+            echo "- srcChmod [verbose=yes|no] \r\n";
+            echo "- langsToExcels \r\n";
+            echo "\r\n";
+        }
+    }
 
-	/**
-	 * bundle
-	 *
-	 * @param array $args
-	 * @return void
-	 */
-	public static function bundle(array $args)
-	{
+    /**
+     * langsToExcels
+     *
+     * @param array $args
+     * @return void
+     */
+    public static function langsToExcels(array $args)
+    {
+        $langsDir = realpath(__DIR__ . '/../src/app/lang');
+        $dirMapper = new DirectoryObject($langsDir, $langsDir);
 
-		$toZip = isset($args['zip']) ? $args['zip'] : 'no';
-		$toZip = $toZip == 'yes' ? true : false;
+        $dirMapper->process();
 
-		$verbose = isset($args['verbose']) ? $args['verbose'] : 'no';
-		$verbose = $verbose == 'yes' ? true : false;
+        $files = $dirMapper->getFiles();
+        $langsData = [];
+        $langs = [];
 
-		$directory = new DirectoryObject(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src');
-		$ignored = [];
+        foreach ($files as $file) {
 
-		$directory->process(new FilesIgnore([
-			'src/vendor',
-			'src/node_modules',
-			'gulpfile.js',
-			'package.json',
-			'package-lock.json',
-			'composer.lock',
-			'sass',
-		]), $ignored);
+            $name = $file->getBasename();
+            $onlyName = pathinfo($name, \PATHINFO_FILENAME);
+            $extension = pathinfo($name, \PATHINFO_EXTENSION);
 
-		if ($verbose) {
-			echo "Ignored\r\n";
-			var_dump($ignored);
-		}
+            if ($extension == 'php') {
 
-		$result = $directory->copyTo(__DIR__ . DIRECTORY_SEPARATOR . 'output/bundle', $toZip);
+                $langsData[$onlyName] = include $file->getPath();
+                $langs[] = $onlyName;
 
-		if ($verbose) {
-			echo "Copied\r\n";
-			var_dump($result);
-		}
-	}
+            }
 
-	/**
-	 * srcChmod
-	 *
-	 * @param array $args
-	 * @return void
-	 */
-	public static function srcChmod(array $args)
-	{
+        }
 
-		$verbose = isset($args['verbose']) ? $args['verbose'] : 'no';
-		$verbose = $verbose == 'yes' ? true : false;
+        foreach ($langs as $lang) {
 
-		$directory = new DirectoryObject(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src');
-		$ignored = [];
+            $spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
+            $first = true;
+            $added = [];
 
-		$directory->process(new FilesIgnore([
-			'INCLUDE_EXPR::^src/app$',
-			'INCLUDE_EXPR::^src/app/logs$',
-			'src/app',
-			'src/statics/plugins',
-			'src/vendor',
-			'src/node_modules',
-			'gulpfile.js',
-			'package.json',
-			'package-lock.json',
-			'composer.lock',
-			'sass',
-		]), $ignored);
+            foreach ($langsData[$lang] as $group => $messages) {
 
-		if ($verbose) {
-			echo "Ignored\r\n";
-			var_dump($ignored);
-		}
+                if ($first) {
 
-		$result = $directory->chmod(0777, DirectoryObject::CHMOD_LEVEL_ALL, true);
+                    $sheet = $spreadsheet->getActiveSheet();
+                    $first = false;
 
-		if ($verbose) {
-			echo "Changed\r\n";
-			var_dump($result);
-		}
-	}
+                } else {
+
+                    $sheet = $spreadsheet->createSheet();
+
+                }
+
+                $sheet->setTitle($group);
+
+                $sheet->setCellValue('A1', 'Nombre');
+                $sheet->setCellValue('B1', 'Mensaje');
+
+                $row = 2;
+
+                foreach ($messages as $name => $message) {
+
+                    $validAdded = $group . '::' . $name;
+
+                    if (!in_array($validAdded, $added)) {
+
+                        $sheet->setCellValue("A{$row}", $name);
+                        $sheet->setCellValue("B{$row}", $message);
+
+                        $added[] = $validAdded;
+                        $row++;
+
+                    }
+
+                }
+
+            }
+
+            $writer = new \PhpOffice\PhpSpreadsheet\Writer\Xlsx($spreadsheet);
+            $writer->save($langsDir . "/{$lang}.xlsx");
+
+        }
+
+    }
+
+    /**
+     * bundle
+     *
+     * @param array $args
+     * @return void
+     */
+    public static function bundle(array $args)
+    {
+
+        $toZip = isset($args['zip']) ? $args['zip'] : 'no';
+        $toZip = $toZip == 'yes' ? true : false;
+
+        $verbose = isset($args['verbose']) ? $args['verbose'] : 'no';
+        $verbose = $verbose == 'yes' ? true : false;
+
+        $directory = new DirectoryObject(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src');
+        $ignored = [];
+
+        $directory->process(new FilesIgnore([
+            'src/vendor',
+            'src/node_modules',
+            'gulpfile.js',
+            'package.json',
+            'package-lock.json',
+            'composer.lock',
+            'sass',
+        ]), $ignored);
+
+        if ($verbose) {
+            echo "Ignored\r\n";
+            var_dump($ignored);
+        }
+
+        $result = $directory->copyTo(__DIR__ . DIRECTORY_SEPARATOR . 'output/bundle', $toZip);
+
+        if ($verbose) {
+            echo "Copied\r\n";
+            var_dump($result);
+        }
+    }
+
+    /**
+     * srcChmod
+     *
+     * @param array $args
+     * @return void
+     */
+    public static function srcChmod(array $args)
+    {
+
+        $verbose = isset($args['verbose']) ? $args['verbose'] : 'no';
+        $verbose = $verbose == 'yes' ? true : false;
+
+        $directory = new DirectoryObject(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src');
+        $ignored = [];
+
+        $directory->process(new FilesIgnore([
+            'INCLUDE_EXPR::^src/app$',
+            'INCLUDE_EXPR::^src/app/logs$',
+            'src/app',
+            'src/statics/plugins',
+            'src/vendor',
+            'src/node_modules',
+            'gulpfile.js',
+            'package.json',
+            'package-lock.json',
+            'composer.lock',
+            'sass',
+        ]), $ignored);
+
+        if ($verbose) {
+            echo "Ignored\r\n";
+            var_dump($ignored);
+        }
+
+        $result = $directory->chmod(0777, DirectoryObject::CHMOD_LEVEL_ALL, true);
+
+        if ($verbose) {
+            echo "Changed\r\n";
+            var_dump($result);
+        }
+    }
 }
