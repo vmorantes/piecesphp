e955878b2198b7011955440b5e19969d564c79b7
vmorantes <vicsen.mb@gmail.com>
2020-11-01 13:48:13 -0500

    -

--- src/app/lang/es.php
+++ src/app/lang/es.php
@@ -3,44 +3,6 @@
 namespace App\LangMessages;
 
 //Hereda los mensajes que estan en lang/default.php
-$lang = [
-	'public' => [
-		'Publicaciones'                                                        => 'Publicaciones',
-		'{DAY_NUMBER} de {MONTH_NAME}, {YEAR}'                                 => '{DAY_NUMBER} de {MONTH_NAME}, {YEAR}',
-		'Quiénes somos'                                                        => 'Quiénes somos',
-		'Mensaje'                                                              => 'Mensaje',
-		'El mensaje ha sido enviado.'                                          => 'El mensaje ha sido enviado.',
-		'Ha ocurrido un error desconocido, intente más tarde.'                 => 'Ha ocurrido un error desconocido, intente más tarde.',
-		'Ha ocurrido un error desconocido al procesar los valores ingresados.' => 'Ha ocurrido un error desconocido al procesar los valores ingresados.',
-		"Fue contactado desde: <a href='%s'>%s</a>"                            => "Fue contactado desde: <a href='%s'>%s</a>",
-		'Contacto'                                                             => 'Contacto',
-		'Ejemplo de tabs'                                                      => 'Ejemplo de tabs',
-		'Nombre'                                                               => 'Nombre',
-		'Correo electrónico'                                                   => 'Correo electrónico',
-		'Marque si desea recibir actualizaciones a su correo electrónico'      => 'Marque si desea recibir actualizaciones a su correo electrónico',
-		'Asunto'                                                               => 'Asunto',
-		'Enviar'                                                               => 'Enviar',
-		'Limpiar'                                                              => 'Limpiar',
-		'Menú'                                                                 => 'Menú',
-		'Página principal'                                                     => 'Página principal',
-		'Blog'                                                                 => 'Blog',
-		'Publicaciones recientes'                                              => 'Publicaciones recientes',
-		'Más'                                                                  => 'Más',
-		'Contáctenos'                                                          => 'Contáctenos',
-		'Información de contacto'                                              => 'Información de contacto',
-		'Teléfono'                                                             => 'Teléfono',
-		'Fax'                                                                  => 'Fax',
-		'Todos los derechos reservados'                                        => 'Todos los derechos reservados',
-		'Diseño'                                                               => 'Diseño',
-		'E-mail'                                                               => 'E-mail',
-		'Acepta que le envíen actualizaciones'                                 => 'Acepta que le envíen actualizaciones',
-		'Sí'                                                                   => 'Sí',
-		'No'                                                                   => 'No',
-		'Características'                                                      => 'Características',
-		'Relacionadas'                                                         => 'Relacionadas',
-		'Ver más'                                                              => 'Ver más',
-		'Categorías'                                                           => 'Categorías',
-	],
-];
+$lang = [];
 
 return $lang;
