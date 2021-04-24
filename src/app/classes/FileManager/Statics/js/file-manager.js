c82e1c342be24e1216226a2abf718fd52e5a0555
vmorantes <vicsen.mb@gmail.com>
2021-04-24 18:51:31 -0500

    -

--- src/app/classes/FileManager/Statics/js/file-manager.js
+++ src/app/classes/FileManager/Statics/js/file-manager.js
@@ -1,10 +1,11 @@
 /// <reference path="../../../../../statics/core/js/configurations.js" />
 /// <reference path="../../../../../statics/core/js/helpers.js" />
 window.addEventListener('load', function () {
-
+	
 	let fileManagerSelector = '.filemanager-component'
 	let fileManagerComponent = $(fileManagerSelector)
 
+	//https://github.com/Studio-42/elFinder/wiki/Client-configuration-options
 	$(fileManagerSelector).elfinder({
 		url: fileManagerComponent.data('route'),
 		baseUrl: fileManagerComponent.data('base-url') + '/',
