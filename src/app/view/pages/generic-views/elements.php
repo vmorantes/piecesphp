de15929b6ec82a8786282491f6977542e2ca2d4c
Vicsen Morantes <vicsen.mb@gmail.com>
2022-03-07 16:14:41 -0500

    -

--- src/app/view/pages/generic-views/elements.php
+++ src/app/view/pages/generic-views/elements.php
@@ -3,7 +3,7 @@
 
     <div class="content">
 
-        <div class="wrapper">
+        <div class="wrapper medium-size ">
 
             <h1 class="segment-title text-center"><?= __(LANG_GROUP, 'Elements'); ?></h1>
 
@@ -13,7 +13,7 @@
 
     <div class="content">
 
-        <div class="wrapper">
+        <div class="wrapper medium-size ">
 
             <h2 class="text-center">Tabs</h2>
 
@@ -46,6 +46,57 @@
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, suscipit provident, ab, quisquam fugit error sunt deserunt architecto blanditiis ipsum ut! Libero hic commodi distinctio obcaecati nisi numquam rem laboriosam?</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, suscipit provident, ab, quisquam fugit error sunt deserunt architecto blanditiis ipsum ut! Libero hic commodi distinctio obcaecati nisi numquam rem laboriosam?</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, suscipit provident, ab, quisquam fugit error sunt deserunt architecto blanditiis ipsum ut! Libero hic commodi distinctio obcaecati nisi numquam rem laboriosam?</p>
+        </div>
+
+    </div>
+
+    <div class="content">
+
+        <div class="wrapper medium-size ">
+
+            <h2 class="text-center"><?= __(LANG_GROUP, 'Título de sección'); ?></h2>
+            <div class="segment-title">Lorem, ipsum dolor. (Normal)</div>
+            <div class="segment-title text-center">Lorem, ipsum dolor. (Centrado)</div>
+            <div class="segment-title text-right">Lorem, ipsum dolor. (A la derecha)</div>
+            <div class="segment-title medium">Lorem, ipsum dolor. (medium)</div>
+        </div>
+
+    </div>
+
+    <div class="content">
+
+        <div class="wrapper medium-size ">
+
+            <h2 class="text-center"><?= __(LANG_GROUP, 'Listas'); ?></h2>
+
+            <h3 class="text-center"><?= __(LANG_GROUP, 'Ordenadas'); ?></h3>
+
+            <ol class="custom-list">
+                <li>Lorem, ipsum dolor.</li>
+                <li>Provident, doloremque. Fugiat!</li>
+                <ol class="custom-list">
+                    <li>Lorem, ipsum dolor.</li>
+                    <li>Provident, doloremque. Fugiat!</li>
+                    <li>Vitae, tempora adipisci?</li>
+                    <li>Ut, inventore magni?</li>
+                </ol>
+                <li>Ut, inventore magni?</li>
+            </ol>
+
+            <h3 class="text-center"><?= __(LANG_GROUP, 'Desordenadas'); ?></h3>
+
+            <ul class="custom-list">
+                <li>Lorem, ipsum dolor.</li>
+                <li>Provident, doloremque. Fugiat!</li>
+                <ul class="custom-list">
+                    <li>Lorem, ipsum dolor.</li>
+                    <li>Provident, doloremque. Fugiat!</li>
+                    <li>Vitae, tempora adipisci?</li>
+                    <li>Ut, inventore magni?</li>
+                </ul>
+                <li>Ut, inventore magni?</li>
+            </ul>
+
         </div>
 
     </div>
